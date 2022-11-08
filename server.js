4c0d3ed20a591a680c462549288e630bbe376330
Kitae's Github <aqua0405@gmail.com>
2022-11-09 02:03:40 +0900

    xterm.js를 이용한 채팅

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 import {Server, Room} from "colyseus";
+import {WebSocketTransport} from "@colyseus/ws-transport";
+import express from "express";
+import cors from "cors";
+import http from "http";
+import chatroom from "./chatroom.js"
+
 
 // 포트 설정 
 const port = process.env.port || 3000
+const app = express()
+
+// 미들웨어 
+app.use(cors());
+app.use(express.json());
 
 // 게임 서버 만들기 
-const gameServer = new Server();
+const gameServer = new Server({
+  transport: new WebSocketTransport({
+    server: http.createServer(app)
+  })
+});
 
 // kitae 라 불리는 방 만들기 
-gameServer.define("kitae", Room);
+gameServer.define("kitae", chatroom);
+
+// public 폴더 설정 
+app.use(express.static("public"));
 
 // listen
 gameServer.listen(port);
